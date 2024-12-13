0da370470d73f85ff506d76ff5b6ac3afca88fe1
mqWork <mqithelp@gmail.com>
2024-12-13 17:00:16 +0300

    init commit. Добавлена модель Avatar с требуемыми полями и аннотациями.

---
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,57 @@
+package ru.hogwarts.school.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    private byte[] data;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Student student;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
