0a240cfa5bfb4ed1bad8e3fec0449542ed46203a
MaxHome <mqithelp@gmail.com>
2024-12-20 22:38:16 +0300

    Step one is done.

--- src/main/java/ru/hogwarts/school/scripts421.sql
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -3,3 +3,10 @@ ALTER TABLE student
 
 ALTER TABLE student
     ALTER COLUMN age SET DEFAULT 20;
+
+ALTER TABLE student
+    ADD CONSTRAINT name_unique UNIQUE (name),
+    ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE faculty
+    ADD CONSTRAINT faculty_unique UNIQUE (color, name);
