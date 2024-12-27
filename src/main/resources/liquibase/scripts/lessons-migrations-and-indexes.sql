67a3b3f7b75c77d5725b1282aee8268bfadb32df
MaxHome <mqithelp@gmail.com>
2024-12-27 23:23:52 +0300

    Недочеты исправлены

--- src/main/resources/liquibase/scripts/lessons-migrations-and-indexes.sql
+++ src/main/resources/liquibase/scripts/lessons-migrations-and-indexes.sql
@@ -1,7 +1,10 @@
 -- liquibase formatted sql
 
--- changeset addIndexForSearchStudent:1
-CREATE INDEX student_search_index ON student (name);
+-- changeset addIndexForSearchStudent:1 runOnChange=true
+CREATE INDEX student_name_index ON student (name);
 
--- changeset addIndexForSearchColorFaculty:2
-CREATE INDEX faculty_color_search_index ON faculty (color);
+-- changeset addIndexForSearchColorFaculty:2 runOnChange=true
+CREATE INDEX faculty_color_index ON faculty (color);
+
+-- changeset addIndexForNameFaculty:3 runOnChange=true
+CREATE INDEX faculty_name_index ON faculty (name);
