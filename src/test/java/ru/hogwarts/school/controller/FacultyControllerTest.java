4848e2617fe2406316c9982326a7457ec6e8475d
MaxHome <mqithelp@gmail.com>
2025-01-07 23:26:52 +0300

    init commit

---
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -0,0 +1,36 @@
+package ru.hogwarts.school.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacultyControllerTest {
+
+    @Test
+    void addFaculty() {
+    }
+
+    @Test
+    void readFaculty() {
+    }
+
+    @Test
+    void updateFaculty() {
+    }
+
+    @Test
+    void delFaculty() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByColorOrName() {
+    }
+
+    @Test
+    void facultyByStudents() {
+    }
+}
---
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -0,0 +1,40 @@
+package ru.hogwarts.school.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void readStudent() {
+    }
+
+    @Test
+    void editStudent() {
+    }
+
+    @Test
+    void delStudent() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByAge() {
+    }
+
+    @Test
+    void findByAgeBetween() {
+    }
+
+    @Test
+    void studentsByFaculty() {
+    }
+}
