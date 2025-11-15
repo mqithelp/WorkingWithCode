63dec6ae45b7c4848a8af3b3d9c5fc89d98a268b
Max-Home <mqithelp@yandex.ru>
2025-11-15 15:14:58 +0300

    init commit for Task 1 Задание 1 Шаг 1 Создать класс для тестирования в пакете test. Создать по одному тесту на каждый эндпоинт контроллера StudentController, используя TestRestTemplate. Шаг 2 Создать еще один класс для тестирования в пакете test. Создать по одному тесту на каждый эндпоинт контроллера FacultyController, используя TestRestTemplate.

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,39 @@
-package ru.hogwarts.school;
+    package ru.hogwarts.school;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+    import org.junit.jupiter.api.Assertions;
+    import org.junit.jupiter.api.Test;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.test.context.SpringBootTest;
+    import org.springframework.boot.test.web.client.TestRestTemplate;
+    import org.springframework.boot.test.web.server.LocalServerPort;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import ru.hogwarts.school.controller.StudentController;
 
-@SpringBootTest
-class SchoolApplicationTests {
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    class SchoolApplicationTests {
+
+   @LocalServerPort
+    private int port;
+
+    @Autowired
+        private StudentController studentController;
+
+    @Autowired
+        private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception{
+        Assertions.assertNotNull(studentController);
+    }
+    @Test
+        public void testGetAllStudents() throws Exception{
+        String url = "http://localhost:" + port + "/student/count";
+        ResponseEntity<Long> response = restTemplate.getForEntity(url, Long.class);
+        //Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        //Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(20, response.getBody()); // Достаем значение по ключу
+
     }
 
-}
+    }
